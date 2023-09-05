fa259f2e524462abcbedcfb4ca5eae87a88ec570
KienDev <ngotrungkien2k2@gmail.com>
2023-09-05 10:48:21 +0700

    product API

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,125 @@
+﻿using AutoMapper;
+using Azure;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Cryptography.Xml;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+        private readonly ResponsDTO _responsDTO;
+        private readonly IMapper _mapper;
+
+        public ProductAPIController(AppDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            _responsDTO = new ResponsDTO();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public ResponsDTO Get()
+        {
+            try
+            {
+                IEnumerable<Product> coupons = _context.Products.ToList();
+                _responsDTO.Result = _mapper.Map<IEnumerable<ProductDto>>(coupons);
+            }
+            catch (Exception ex)
+            {
+                _responsDTO.IsSuccess = false;
+                _responsDTO.Message = ex.Message;
+            }
+
+            return _responsDTO;
+        }
+
+
+        [HttpGet("{id}")]
+        public ResponsDTO Get(int id)
+        {
+            try
+            {
+                Product coupons = _context.Products.First(el => el.ProductId == id);
+                _responsDTO.Result = _mapper.Map<ProductDto>(coupons);
+            }
+            catch (Exception ex)
+            {
+                _responsDTO.IsSuccess = false;
+                _responsDTO.Message = ex.Message;
+            }
+
+            return _responsDTO;
+        }
+
+
+        [HttpPost]
+        public ResponsDTO Post([FromBody] ProductDto couponDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(couponDto);
+                _context.Add(obj);
+                _context.SaveChanges();
+                _responsDTO.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+
+                _responsDTO.IsSuccess = false;
+                _responsDTO.Message = ex.Message;
+            }
+            return _responsDTO;
+        }
+
+        [HttpPut]
+        public ResponsDTO Put([FromBody] ProductDto couponDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(couponDto);
+                _context.Products.Update(obj);
+                _context.SaveChanges();
+
+                _responsDTO.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _responsDTO.IsSuccess = false;
+                _responsDTO.Message = ex.Message;
+            }
+            return _responsDTO;
+        }
+
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ResponsDTO Delete(int id)
+        {
+            try
+            {
+                Product obj = _context.Products.First(u => u.ProductId == id);
+                _context.Products.Remove(obj);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _responsDTO.IsSuccess = false;
+                _responsDTO.Message = ex.Message;
+            }
+            return _responsDTO;
+        }
+    }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,11 +1,59 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+////ntkien - config
+// add DB context
+builder.Services.AddDbContext<AppDBContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// config mapper 2 object với nhau
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference= new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id=JwtBearerDefaults.AuthenticationScheme
+                }
+            }, new string[]{}
+        }
+    });
+});
+
+builder.AddAppAuthetication();
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,8 +66,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+// mặc định khi run thì nó sẽ kiểm tra có migration nào đang pending ko? nếu có thì nó cập nhật trong db
+ApplyMigration();
 
 app.MapControllers();
 
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDBContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
